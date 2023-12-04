c26293f94a700a0ec22b2e1bd5ecf93f2e981d4d
User <elxan.memmedli2003@gmail.com>
2023-12-05 03:38:38 +0400

    First commit

---
+++ Extension_Helper/Models/Extension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Extension_Helper.Models
+{
+    internal static class Extension
+    {
+        public static (bool, string) IsOdd(this int num)
+        {
+            if (num is 0) return (false,"Daxil etdiyiniz eded ne tek ne de cutdur") ;
+            else if (num % 2 is 0) return (false,"Bu eded tek deyil"); ;
+            return (true,"Bu eded tekdir");
+        }
+
+        public static (bool,string) IsEven(this int num) 
+        {
+            if (num is 0) return (false,"Daxil etdiyiniz eded ne tek ne de cutdur") ;
+            else if (num % 2 is 0) return (true, "Bu eded cutdur");
+            return (false,"Bu eded cut deyil");
+        }
+
+        public static (bool,string) HasDigit(this string str)
+        {
+            foreach (char item in str)
+            {
+                if (char.IsDigit(item)) return (true, "Bu sozun terkibinde reqem var");
+            }
+            return (false, "Bu sozun terkibinde reqem yoxdur");
+        }
+
+        public static string ToCapitalize(this string str)
+        {
+            if (str.Trim() is null || str.Trim() is "")
+            {
+                return "Bos gonderile bilmez";
+            }
+            str = str.Trim();   
+            return str[0].ToString().ToUpper() + str.Substring(1).ToLower();
+        }
+
+        public static string ToCapitalize2(this string str)
+        {
+            if (str.Trim() is null || str.Trim() is "")
+            {
+                return "Bos gonderile bilmez";
+            }
+            StringBuilder newstr = new StringBuilder();
+            return newstr.Append(char.ToUpper(str[0])).Append(str.Substring(1).ToLower()).ToString();
+        }
+    }
+}
---
+++ Extension_Helper/Program.cs
@@ -0,0 +1,26 @@
+﻿using Extension_Helper.Models;
+
+namespace Extension_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Aşağıdakı metodlardan ibarət static Extention class-ı yaradın.
+
+
+            IsOdd() - int obyektlər üçün həmin obyektin tək ədəd olub olmadığını geri qaytaran (true/false) method
+
+
+            IsEven() - int obyektlər üçün həmin obyektin cüt ədəd olub olmadığını geri qaytaran (true/false) method
+
+
+            HasDigit() - string obyektlər üçün həmin obyektin daxilində digit character olub olmamasını geri qaytaran(true/false) metod
+
+
+            ToCapitalize() - String-lər üçün ToCapitalize adlı extention metod yaradırsınız.Bu metod string dəyərini capitalaize edib geri qaytarmalıdır.
+             */
+        }
+    }
+}
